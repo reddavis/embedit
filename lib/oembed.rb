913b8640b646958df974e8f86f030351424c3dff
Red Davis <reddavis@gmail.com>
2008-07-31 17:47:05 +0100

    commented out examples

--- lib/oembed.rb
+++ lib/oembed.rb
@@ -9,14 +9,14 @@ require 'oembed/providers'
 require 'oembed/media'
 
 
-puts a = Oembed::Media.new('http://www.vimeo.com/1263763').title
+#puts a = Oembed::Media.new('http://www.vimeo.com/1263763').title
 
-puts b = Oembed::Media.new('http://www.flickr.com/photos/davidgutierrez/2135724493/').title
+#puts b = Oembed::Media.new('http://www.flickr.com/photos/davidgutierrez/2135724493/').title
 
-puts c = Oembed::Media.new('http://www.viddler.com/explore/winelibrarytv/videos/142/').title
+#puts c = Oembed::Media.new('http://www.viddler.com/explore/winelibrarytv/videos/142/').title
 
-puts d = Oembed::Media.new('http://qik.com/video/141977').title
+#puts d = Oembed::Media.new('http://qik.com/video/141977').title
 
-puts e = Oembed::Media.new('http://pownce.com/dburka/notes/2951118/').title
+#puts e = Oembed::Media.new('http://pownce.com/dburka/notes/2951118/').title
 
-puts f = Oembed::Media.new('http://revision3.com/trs/blockoland/').html
+#puts f = Oembed::Media.new('http://revision3.com/trs/blockoland/').html
