008f2d192d1d39e37fab8bf4f04f59772bfc69b0
Red Davis <reddavis@gmail.com>
2008-08-26 16:02:24 +0100

    change examples

--- lib/embedit.rb
+++ lib/embedit.rb
@@ -6,26 +6,26 @@ require 'open-uri'
 require 'yaml'
 
 #Files
-require 'oembed/providers'
-require 'oembed/media'
-require 'oembed/oembed'
-require 'oembed/youtube'
+require 'embedit/providers'
+require 'embedit/media'
+require 'embedit/oembed'
+require 'embedit/youtube'
 
 
 #puts a = Embedit::Media.new('http://www.vimeo.com/1263763').format
 
 #puts b = Embedit::Media.new('http://www.flickr.com/photos/davidgutierrez/2135724493/').html(:height => 200)
 
-#puts c = Oembed::Media.new('http://www.viddler.com/explore/winelibrarytv/videos/142/').html(:height => 200, :width => 500)
+#puts c = Embedit::Media.new('http://www.viddler.com/explore/winelibrarytv/videos/142/').html(:height => 200, :width => 500)
 
-#puts d = Oembed::Media.new('http://qik.com/video/141977').html(:height => 50)
+#puts d = Embedit::Media.new('http://qik.com/video/141977').html(:height => 50)
 
-#puts e = Oembed::Media.new('http://pownce.com/dburka/notes/2951118/').html(:height => 200)
+#puts e = Embedit::Media.new('http://pownce.com/dburka/notes/2951118/').html(:height => 200)
 
-#puts f = Oembed::Media.new('http://revision3.com/trs/blockoland/').html(:height => 500)
+#puts f = Embedit::Media.new('http://revision3.com/trs/blockoland/').html(:height => 500)
 
 #YouTUBE
 
-#puts g = Oembed::Media.new("http://www.youtube.com/watch?v=j3TOT1lnVTA")
+#puts g = Embedit::Media.new("http://www.youtube.com/watch?v=j3TOT1lnVTA")
 
-puts Embedit::Media.new("http://www.youtube.com/watch?v=j3TOT1lnVTA")
+#puts Embedit::Media.new("http://www.youtube.com/watch?v=j3TOT1lnVTA")
