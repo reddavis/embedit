7381913cce7e7634a04fe3e277890bd6c34b5a80
Red Davis <reddavis@gmail.com>
2008-08-27 22:04:37 +0100

    flickr tests

--- spec/oembed_spec.rb
+++ spec/oembed_spec.rb
@@ -1,6 +1,6 @@
 require File.dirname(__FILE__) + '/spec_helper.rb'
 
-describe Embedit::Media do
+describe "Vimeo oEmbed tests" do
   
   it "should properly validate a valid Vimeo url" do
     media = Embedit::Media.new('http://www.vimeo.com/1263763').valid?
@@ -14,6 +14,21 @@ describe Embedit::Media do
   
   it "should validate an un-valid (with some letters) Vimeo url" do
     media = Embedit::Media.new('http://www.vimeo.com/126badurl').valid?
+    media.should == false
+  end
+
+end
+
+describe "Flickr oEmbed tests" do
+  
+  it "should properly validate a valid Flickr url" do
+    media = Embedit::Media.new('http://www.flickr.com/photos/asianmack/2781811902/in/set-72157606856535809/').valid?
+    media.should == true
+  end
+  
+  it "should properly validate an un-valid (with only numbers) Flickr url" do
+    media = Embedit::Media.new('http://www.flickr.com/photos/asianmack/2781811902/in/set-72157606856535809/').valid?
+    media.should == true
   end
   
 end
