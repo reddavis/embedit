280c6c4a6a1fc43872c83aaa58eb9d6c147e1ba5
Red Davis <reddavis@gmail.com>
2008-09-04 11:24:59 +0100

    ovi support

--- lib/embedit.rb
+++ lib/embedit.rb
@@ -15,7 +15,7 @@ require 'embedit/exceptions'
 require 'embedit/validate'
 
 
-#puts a = Embedit::Media.new('http://www.vimeo.com/1263763').format
+#puts a = Embedit::Media.new('http://www.vimeo.com/1263763').html
 
 #puts b = Embedit::Media.new('http://www.flickr.com/photos/reddavis999/2692043113/').format #valid? #.valid #.html(:height => 200)
 
@@ -35,4 +35,8 @@ require 'embedit/validate'
 
 #OVI
 
-puts a = Embedit::Media.new('http://share.ovi.com/media/james___.public/james___.10016').title
+#puts a = Embedit::Media.new('http://share.ovi.com/media/ekki808.mu-sick/ekki808.10001').html(:height => 900, :width => 100)
+
+#puts b = Embedit::Media.new('http://share.ovi.com/media/DefragTV.public/DefragTV.10014').html#(:height => 900, :width => 100)
+
+#puts c = Embedit::Media.new('http://share.ovi.com/media/james___.public/james___.10016').html(:height => 900, :width => 100)
--- lib/embedit/ovi.rb
+++ lib/embedit/ovi.rb
@@ -16,14 +16,16 @@ module Embedit
     end
     
     def html(size = {})
+      @html.gsub!(/height="\d+"/, %{height="#{size[:height]}"}) if size[:height]
+      @html.gsub!(/width="\d+"/, %{width="#{size[:width]}"}) if size[:width]
       @html
     end
     
     private
     
     def work_out_html(page)
-      @html = page.search("tr#M_sidebar_uimediaembed_uifp1 td").inner_html          #We first search for video or audio, if not its got to be image (hopfully)
-      if @html.empty?
+      @html = page.search("tr#M_sidebar_uimediaembed_uifp1 td input").first.attributes['value'] rescue nil  #We first search for video or audio, if not its got to be image (hopfully)
+      if @html.nil?
         @html = page.search("div#M_sidebar_uimediaembed_uiip td input#M_sidebar_uimediaembed_uihtml2").first.attributes['value']
       end
     end
